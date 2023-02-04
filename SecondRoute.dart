582adb11070c3e379984d2acf71368b4c77f5e26
Eileen Mascarenhas <93201259+EileenMascarenhas163@users.noreply.github.com>
2023-02-04 16:02:39 +0530

    Add files via upload

---
+++ SecondRoute.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/ThirdRoute.dart';
+
+class MySecondPage extends StatefulWidget {
+  const MySecondPage({super.key});
+
+  @override
+  State<MySecondPage> createState() => _MySecondPageState();
+}
+
+class _MySecondPageState extends State<MySecondPage> {
+  TextEditingController? textController;
+  final _unfocusNode = FocusNode();
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    textController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _unfocusNode.dispose();
+    textController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Color(0xFFEB4343),
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Search a  Recipe for a dish',
+          style: TextStyle(
+            fontFamily: 'Poppins',
+            color: Colors.white,
+            fontSize: 22,
+          ),
+        ),
+        actions: [],
+        centerTitle: false,
+        elevation: 2,
+      ),
+      body: SafeArea(
+        child: GestureDetector(
+          onTap: () => FocusScope.of(context).requestFocus(_unfocusNode),
+          child: // Generated code for this Stack Widget...
+              Stack(
+            children: [
+              SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Container(
+                                width: 200,
+                                child: Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.8,
+                                  child: TextFormField(
+                                    controller: textController,
+                                    autofocus: true,
+                                    obscureText: false,
+                                    decoration: InputDecoration(
+                                      hintText: '[Some hint text...]',
+                                      enabledBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                          color: Color(0xFF04030A),
+                                          width: 1,
+                                        ),
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      focusedBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                          color: Color(0xFF04030A),
+                                          width: 1,
+                                        ),
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      errorBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                          color: Color(0x00000000),
+                                          width: 1,
+                                        ),
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                      focusedErrorBorder: OutlineInputBorder(
+                                        borderSide: BorderSide(
+                                          color: Color(0x00000000),
+                                          width: 1,
+                                        ),
+                                        borderRadius: BorderRadius.circular(5),
+                                      ),
+                                    ),
+                                    // style: FlutterFlowTheme.of(context).bodyText1,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Ink(
+                              decoration: const ShapeDecoration(
+                                color: Color(0xFFEB4343),
+                                shape: CircleBorder(),
+                              ),
+                              child: IconButton(
+                                color: Colors.white,
+                                icon: Icon(
+                                  Icons.search,
+                                ),
+                                iconSize: 50,
+                                onPressed: () {},
+                              )),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                      child: Container(
+                        width: 364.5,
+                        height: 600,
+                        decoration: BoxDecoration(
+                          color: Color(0x71D9D9D9),
+                          borderRadius: BorderRadius.circular(5),
+                          shape: BoxShape.rectangle,
+                        ),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional(-0.01, 0.98),
+                child: Container(
+                  width: 80,
+                  height: 80,
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                  ),
+                  child: GestureDetector(
+                    onTap: () {
+                       Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const MyThirdPage()),
+                    );
+                    },
+                    child: Image.asset(
+                      'assets/images/vegetables.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
